ca13646e0006f14a2877cbe7eb924a72161a88be
Marcel Benders <marcel.benders@cleverbridge.com>
2019-03-22 12:14:31 +0100

    Initial commit

---
+++ Abstract/MeasureContext.cs
@@ -0,0 +1,26 @@
+﻿using com.b_velop.stack.Classes.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace com.b_velop.stack.DataContext.Abstract
+{
+    class MeasureContext : DbContext
+    {
+        private readonly ILogger<MeasureContext> _logger;
+
+        public DbSet<MeasurePoint> MeasurePoints { get; set; }
+        public DbSet<MeasureValue> MeasureValues { get; set; }
+        public DbSet<Unit> Units { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<BatteryState> BatteryStates { get; set; }
+        public DbSet<PriorityState> PriorityStates { get; set; }
+        public DbSet<ActiveMeasurePoint> ActiveMeasurePoints { get; set; }
+
+        public MeasureContext(
+            ILogger<MeasureContext> logger,
+            DbContextOptions<MeasureContext> context) : base(context)
+        {
+            _logger = logger;
+        }
+    }
+}
