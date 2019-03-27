de5c207af5f16f64dcbda4df7fc93e24bf6dd828
Marcel Benders <marcel.benders@cleverbridge.com>
2019-03-27 15:47:40 +0100

    (dev) add new filter type

--- Repository/ITimeDataStore.cs
+++ Repository/ITimeDataStore.cs
@@ -7,5 +7,6 @@ namespace com.b_velop.stack.DataContext.Repository
     public interface ITimeDataStore<TType> where TType : ITimeEntity
     {
         Task<IEnumerable<TType>> FilterPointValuesByTimeAsync(Guid id, int seconds);
+        Task<IEnumerable<TType>> FilterValuesByTimeAsync(int seconds);
     }
 }
